7410f682deabe60c41809352e55d4cdb4e8d027c
chajr <git@bluetree.pl>
2015-01-19 19:07:32 +0100

    add other unit test (collection) (issue #58)
    This version introduces multiple enhancements:
    
    added collection test class
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Added:      CollectionTest.php

---
+++ src/Test/CollectionTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * test Collection Object class
+ *
+ * @package     ClassKernel
+ * @subpackage  Test
+ * @author      Michał Adamiak    <chajr@bluetree.pl>
+ * @copyright   chajr/bluetree
+ */
+namespace Test;
+
+use ClassKernel\Data\Collection;
+
+class CollectionTest extends \PHPUnit_Framework_TestCase
+{
+    
+}
