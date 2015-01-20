c0aad6109bdecc4623c51fa17e77b7bc154d7b31
chajr <git@bluetree.pl>
2015-01-20 20:34:36 +0100

    add other unit test (collection) (issue #58)
    This version introduces multiple enhancements:
    
    added some basic test for collection
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Modified:   CollectionTest.php
                -- Added --
                    testCreateCollection test basic object creation
                    testCreateCollectionWithValidation
                    testCreateCollectionWithDataPreparation
                    testArrayAccessForCollection
                    testPageAccessForCollection
                    testAddDataWithOriginalDataCheck
                    _exampleCollection return some data to test collection functionality

--- src/Test/CollectionTest.php
+++ src/Test/CollectionTest.php
@@ -10,8 +10,117 @@
 namespace Test;
 
 use ClassKernel\Data\Collection;
+use ClassKernel\Data\Object;
 
 class CollectionTest extends \PHPUnit_Framework_TestCase
 {
-    
+    /**
+     * test basic object creation
+     * 
+     * @requires exampleCollection
+     */
+    public function testCreateCollection()
+    {
+        $data = $this->_exampleCollection();
+        $collection = new Collection([
+            'data'  => $data
+        ]);
+
+        $this->assertEquals('lorem ipsum', $collection->first());
+        $this->assertEquals($data[1]['data_first'], $collection->getElement(1)['data_first']);
+
+        $collection = new Collection;
+        $collection->appendArray($data);
+
+        $this->assertEquals('lorem ipsum', $collection->first());
+        $this->assertEquals($data[1]['data_first'], $collection->getElement(1)['data_first']);
+    }
+
+    public function testCreateCollectionWithValidation()
+    {
+        
+    }
+
+    public function testCreateCollectionWithDataPreparation()
+    {
+        
+    }
+
+    public function testArrayAccessForCollection()
+    {
+        
+    }
+
+    public function testPageAccessForCollection()
+    {
+        
+    }
+
+    public function testAddDataWithOriginalDataCheck()
+    {
+        
+    }
+
+    //add create collection with other than array data types
+
+    /**
+     * return some data to test collection functionality
+     * 
+     * @return array
+     */
+    protected function _exampleCollection()
+    {
+        $object = new Object(
+            [
+                'data' => [
+                    'data_first'    => 'first',
+                    'data_second'   => 2,
+                    'data_third'    => false,
+                ]
+            ]
+        );
+        $object2 = clone $object;
+        $object2->destroy();
+        $object2->set([
+            'data_first'    => 1,
+            'data_second'   => 'second',
+            'data_third'    => true,
+        ]);
+
+        return [
+            'lorem ipsum',
+            [
+                'data_first'    => 1,
+                'data_second'   => 2,
+                'data_third'    => 3,
+            ],
+            [
+                'data_first'    => true,
+                'data_second'   => false,
+                'data_third'    => null,
+            ],
+            [
+                'data_first'    => 'first',
+                'data_second'   => 'second',
+                'data_third'    => 'third',
+            ],
+            [
+                'data_first'    => '001',
+                'data_second'   => '002',
+                'data_third'    => '003',
+            ],
+            [
+                'data_first'    => [1, 2, 3],
+                'data_second'   => [4, 5, 6],
+                'data_third'    => [7, 8, 9],
+            ],
+            [
+                'data_first'    => 4,
+                'data_second'   => 5,
+                'data_third'    => 6,
+            ],
+            $object,
+            $object2
+        ];
+    }
 }
