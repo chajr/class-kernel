424a1db3b97dc99b47435e879d6ccf1a468cde19
chajr <git@bluetree.pl>
2015-01-25 12:49:25 +0100

    add other unit test (collection) (issue #58)
    This version introduces multiple enhancements:
    
    modified data providers and add some tests
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Modified:   Test/CollectionTest.php
                -- Added --
                    testPageInformation test correct set page size and count available pages
                    testPageAccessForCollection test access to collection using pages
                    testArrayAccessToCollectionPages
                    exampleCollectionObject create collection object for test
                -- Updated --
                    testCreateCollection modified data provider
                    testArrayAccessForCollection modified data provider
                    testBasicAccessToCollectionElements modified data provider

--- src/Test/CollectionTest.php
+++ src/Test/CollectionTest.php
@@ -16,16 +16,15 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * test basic object creation
-     * 
+     *
+     * @param Collection $collection
+     * @param array $data
+     * @dataProvider exampleCollectionObject
      * @requires exampleCollection
+     * @requires _exampleCollectionObject
      */
-    public function testCreateCollection()
+    public function testCreateCollection($collection, array $data)
     {
-        $data = $this->_exampleCollection();
-        $collection = new Collection([
-            'data'  => $data
-        ]);
-
         $this->assertEquals('lorem ipsum', $collection->first());
         $this->assertEquals($data[1]['data_first'], $collection->getElement(1)['data_first']);
 
@@ -39,15 +38,14 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
     /**
      * check usage collection as array (access data and loop processing)
      *
+     * @param Collection $collection
+     * @param array $data
+     * @dataProvider exampleCollectionObject
      * @requires exampleCollection
+     * @requires _exampleCollectionObject
      */
-    public function testArrayAccessForCollection()
+    public function testArrayAccessForCollection($collection, array $data)
     {
-        $data = $this->_exampleCollection();
-        $collection = new Collection([
-            'data'  => $data
-        ]);
-
         foreach ($collection as $index => $element) {
             $this->assertEquals($data[$index], $element);
         }
@@ -56,21 +54,67 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
     /**
      * test some basic access to single collection elements
      *
+     * @param Collection $collection
+     * @param array $data
+     * @dataProvider exampleCollectionObject
      * @requires exampleCollection
+     * @requires _exampleCollectionObject
      */
-    public function testBasicAccessToCollectionElements()
+    public function testBasicAccessToCollectionElements($collection, array $data)
     {
-        $data = $this->_exampleCollection();
-        $collection = new Collection([
-            'data'  => $data
-        ]);
-
         $this->assertEquals('lorem ipsum', $collection->first());
         $this->assertEquals('lorem ipsum', $collection[0]);
         $this->assertEquals(1, $collection->last()['data_first']);
         $this->assertEquals($data[1]['data_first'], $collection->getElement(1)['data_first']);
         $this->assertEquals(9, $collection->count());
         $this->assertTrue($collection->hasElement(5));
+    }
+
+    /**
+     * test correct set page size and count available pages
+     *
+     * @param Collection $collection
+     * @dataProvider exampleCollectionObject
+     * @requires exampleCollection
+     * @requires _exampleCollectionObject
+     */
+    public function testPageInformation($collection)
+    {
+        $collection->setPageSize(2);
+        $this->assertEquals(2, $collection->getPageSize());
+        $this->assertEquals(5, $collection->countPages());
+    }
+
+    /**
+     * test access to collection using pages
+     *
+     * @param Collection $collection
+     * @param array $data
+     * @dataProvider exampleCollectionObject
+     * @requires exampleCollection
+     * @requires _exampleCollectionObject
+     */
+    public function testPageAccessForCollection($collection, array $data)
+    {
+        $collection->setPageSize(2);
+        $this->assertEquals(2, count($collection->getFirstPage()));
+        $this->assertEquals(1, count($collection->getLastPage()));
+        $this->assertEquals([$data[0], $data[1]], $collection->getFirstPage());
+        $this->assertEquals([$data[8]], $collection->getLastPage());
+        $this->assertEquals([$data[2], $data[3]], $collection->getPage(2));
+        $this->assertNull($collection->getPage(10));
+        $this->assertEquals(1, $collection->getCurrentPage());
+
+        $collection->nextPage();
+
+        $this->assertEquals(2, $collection->getCurrentPage());
+        $this->assertEquals([$data[4], $data[5]], $collection->getNextPage());
+        $this->assertEquals([$data[0], $data[1]], $collection->getPreviousPage());
+    }
+
+    public function testArrayAccessToCollectionPages()
+    {
+        
     }
 
     public function testCreateCollectionWithValidation()
@@ -93,17 +137,28 @@ class CollectionTest extends \PHPUnit_Framework_TestCase
         
     }
 
-    public function testPageAccessForCollection()
-    {
-        
-    }
-
     public function testAddDataWithOriginalDataCheck()
     {
         
     }
 
     //add create collection with other than array data types
+
+    /**
+     * create collection object for test
+     * 
+     * @return Collection
+     */
+    public function exampleCollectionObject()
+    {
+        $data = $this->_exampleCollection();
+        return [[
+            new Collection([
+                'data'  => $data
+            ]),
+            $data
+        ]];
+    }
 
     /**
      * return some data to test collection functionality
